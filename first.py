e7bbe3c5a8d7a43f6be6be00f5974c99ca6dfbe6
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-31 14:00:28 +0330

    sadeghi

--- first.py
+++ first.py
@@ -1,1 +1,91 @@
-print("welcome to our python class")
+# print("welcome to our python class")
+
+# variable
+
+# x = 10
+# print(type(x))
+
+# x = "sara"
+# print(type(x))
+
+# هر ترکیبی از حروف الفبای اانگلیسی و اعداد و آندرلاین
+# با این استثنا که اولین کاراکتر نمی تواند عدد باشد
+
+# number_one = 12
+# _number_one = 12
+# number1 = 12
+# 1number = 12 # error
+
+# number one = 12 # error
+
+# number-one = 12 # error
+
+# some other conventions on define a variable
+# number_one = 1  # snake case name
+# numberOne = 1  # cammel
+
+
+# python primitive data types
+
+# numbers    str    bool
+# numbers   int   float   complex
+
+# x = 1
+# print(type(x))
+# x = 1.2
+# print(type(x))
+# x = 1 + 2j
+# print(type(x))
+
+# arithmetic
+# +   -    *     /    **    //    %
+
+# print(5/2)
+# print(5//2)
+# print(5%2)
+
+
+# str
+# name = 'ali'
+# print(type(name))
+# name = ''
+# print(type(name))
+# name = ' '
+# print(type(name))
+# name = ""
+# print(type(name))
+
+# Scape sequences
+# print('welcome\nto\npython')
+# print('welcome\tpython')
+# print('c:\windows\\newfolder')
+
+# print('sdfbhskhfkshfkjsahfkasfhkjasdfhllllllllllllllllllllllllllllllllllllllllllllllll',
+#       'asdasddddddddddddddd')
+
+
+# print('sum is:', 7+3)
+
+# print('Hello every body.\nwelcome to our python class\npython from zero to hero')
+# using tripple quote
+# print('''Hello every body.
+# welcome to our python class.
+# python from zero to hero''')
+# print("""Hello every body.
+# welcome to our python class.
+# python from zero to hero""")
+
+# print('Display \'hi\' in quotes')
+# print("Display 'hi' in quotes")
+# print('''Display 'hi' in quotes''')
+# print("""Display 'hi' in quotes""")
+
+# name = input("what's your name? ")
+# print(type(name))
+# print(name)
+
+
+value1 = input('Enter first number: ')
+value2 = input('Enter second number: ')
+
+print(int(value1) + int(value2))
