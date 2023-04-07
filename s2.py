c680c9c99f33aae651568cd50cfb8cd0f7940c03
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-07 14:58:04 +0330

    sadeghi

---
+++ s2.py
@@ -0,0 +1,91 @@
+# a, b, c = 1,2,3
+# print(a,b,c)
+
+# a,b,_ = 1,2,3
+
+
+# a = b = c = 1
+# print(a,b,c)
+# b = 2
+# print(a,b,c)
+
+
+# x = y = [1, 2, 3]
+# x = [13, 8, 9]
+# print(y)
+# print(x)
+# x = y = [7, 8, 9]
+
+# x[0] = 13
+
+# print(x)
+# print(y)
+
+# x = [1, 2, [3, 4, 5], 6, 7]
+# print(x[2])
+# print(x[2][1])
+
+# a = 2
+# print(a)
+
+# a = "new value"
+# print(a)
+
+# def my_function():
+#     a = 2
+#     return a
+
+# print(my_function())
+# a = 2
+# b = 3
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# if a > b:
+#     print(a)
+#     print("balalal")
+# pass #
+# def will_be_implemeted_later():
+#     pass
+
+# bool => True    False
+
+# logical operators
+# and   or    not
+
+#   x or y    if x is False then y   otherwise  x
+
+# x = True
+# y = True
+# print(x or y)
+# x = True
+# y = False
+# print(x or y)
+# x = False
+# y = True
+# print(x or y)
+
+
+# x = True or False
+# print(x)
+# print(False or True)
+# print(True or True)
+# print(False or False)
+
+# and
+
+# print(True and True)
+# print(True and False)
+# print(False and False)
+# print(False and True)
+
+
+# truthy and falsy
+print('' or 'mohammad')
+print(0 or 2)
+# تمام اعداد غیر از صفر
+# Truthy
+# صفر   Falsy
+# ''  => False
